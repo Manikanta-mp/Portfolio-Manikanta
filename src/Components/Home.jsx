5fc69efed12f363858a4e9d187616494f19c6851
Manikanta-mp <manikantabadigar@gmail.com>
2025-02-25 15:30:03 +0530

    updated upto styling

---
+++ src/Components/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import img from '../assets/MK.jpg'
+
+const Home = () => {
+  return (
+    <>
+      <div className="container-fluid home-container">
+        <div className="row align-items-center">
+          <div className="col-12 col-md-6 text-section">
+            <h1 className="title">Hi, This is Manikanta</h1>
+            <h5 className="subtitle">And I'm A MernStack Developer</h5>
+            <p className="description">
+              Passionate about building amazing web applications with cutting-edge technologies.
+            </p>
+          </div>
+          <div className="col-12 col-md-6 text-center image-section">
+            <img src={img} alt="Profile-photo" className="profile-img img-fluid" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
